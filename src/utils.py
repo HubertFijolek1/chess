9464e2b307e0a70ad21053e2de401be42e775f96
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-09-14 11:02:56 +0200

    Applied PEP 8 standards; added type annotations. - utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
-def parse_position(pos):
-    col = ord(pos[0]) - ord('a')
+def parse_position(pos: str) -> tuple:
+    """
+    Parses a position string (e.g., 'e2') and converts it to board coordinates.
+    """
+    col = ord(pos[0].lower()) - ord('a')
     row = 8 - int(pos[1])
     return row, col
