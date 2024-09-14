cd3c76e462582ed60d3fabb6cb685fa50cfdd586
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-09-14 10:58:59 +0200

    Renamed the method can_move_to to can_capture_or_move_to for clarity and adjust is_valid_move methods to use is_path_clear

--- src/chess_piece.py
+++ src/chess_piece.py
@@ -1,27 +1,23 @@
-class ChessPiece:
-    def __init__(self, color):
-        self.color = color  # Color of the piece ('white' or 'black')
+from constants import WHITE, BLACK
 
-    def __str__(self):
+class ChessPiece:
+    def __init__(self, color: str):
+        self.color = color  # Color of the piece (WHITE or BLACK)
+
+    def __str__(self) -> str:
         return 'Piece'  # Default string representation of a piece
 
-    def can_move_to(self, end, board):
+    def can_capture_or_move_to(self, end: tuple, board) -> bool:
         """
-        Checks if the piece can move to the given end position.
-        
-        Parameters:
-        end (tuple): End position (row, col)
-        board (list): Current state of the board
-        
-        Returns:
-        bool: True if the move is valid, False otherwise
+        Checks if the piece can move to the given end position or capture an opponent's piece there.
         """
         end_row, end_col = end
-        if board[end_row][end_col] is None:
-            return True  # Move is valid if the end position is empty
-        if board[end_row][end_col].color != self.color:
-            return True  # Move is valid if capturing an opponent's piece
-        return False  # Move is invalid if the end position has a piece of the same color
+        target_piece = board.grid[end_row][end_col]
+        if target_piece is None:
+            return True  # The end position is empty
+        if target_piece.color != self.color:
+            return True  # Can capture opponent's piece
+        return False  # Can't move to a position occupied by own piece
 
 class King(ChessPiece):
     def __str__(self):
@@ -45,51 +41,43 @@ class King(ChessPiece):
         col_diff = abs(start_col - end_col)
         return max(row_diff, col_diff) == 1 and self.can_move_to(end, board)  # King moves one square in any direction
 
-class Queen(ChessPiece):
-    def __str__(self):
-        return 'Q' if self.color == 'white' else 'q'  # String representation for Queen
-
-    def is_valid_move(self, start, end, board):
+class Rook(ChessPiece):
+    def is_valid_move(self, start: tuple, end: tuple, board) -> bool:
         """
-        Checks if the Queen can move from start to end position.
-        
-        Parameters:
-        start (tuple): Start position (row, col)
-        end (tuple): End position (row, col)
-        board (list): Current state of the board
-        
-        Returns:
-        bool: True if the move is valid, False otherwise
+        Checks if the Rook can move from start to end position.
         """
         start_row, start_col = start
         end_row, end_col = end
-        row_diff = abs(start_row - end_row)
-        col_diff = abs(start_col - end_col)
-        if row_diff == col_diff or start_row == end_row or start_col == end_col:
-            return self.can_move_to(end, board)  # Queen moves any number of squares along a row, column, or diagonal
+        if start_row == end_row or start_col == end_col:
+            if board.is_path_clear(start, end) and self.can_capture_or_move_to(end, board):
+                return True  # Rook moves any number of squares along a row or column
         return False
 
 class Bishop(ChessPiece):
-    def __str__(self):
-        return 'B' if self.color == 'white' else 'b'  # String representation for Bishop
-
-    def is_valid_move(self, start, end, board):
+    def is_valid_move(self, start: tuple, end: tuple, board) -> bool:
         """
         Checks if the Bishop can move from start to end position.
-        
-        Parameters:
-        start (tuple): Start position (row, col)
-        end (tuple): End position (row, col)
-        board (list): Current state of the board
-        
-        Returns:
-        bool: True if the move is valid, False otherwise
         """
         start_row, start_col = start
         end_row, end_col = end
-        row_diff = abs(start_row - end_row)
-        col_diff = abs(start_col - end_col)
-        return row_diff == col_diff and self.can_move_to(end, board)  # Bishop moves any number of squares diagonally
+        if abs(end_row - start_row) == abs(end_col - start_col):
+            if board.is_path_clear(start, end) and self.can_capture_or_move_to(end, board):
+                return True  # Bishop moves any number of squares diagonally
+        return False
+
+class Queen(ChessPiece):
+    def is_valid_move(self, start: tuple, end: tuple, board) -> bool:
+        """
+        Checks if the Queen can move from start to end position.
+        """
+        start_row, start_col = start
+        end_row, end_col = end
+        row_diff = end_row - start_row
+        col_diff = end_col - start_col
+        if abs(row_diff) == abs(col_diff) or start_row == end_row or start_col == end_col:
+            if board.is_path_clear(start, end) and self.can_capture_or_move_to(end, board):
+                return True  # Queen moves any number of squares along a row, column, or diagonal
+        return False
 
 class Knight(ChessPiece):
     def __str__(self):
@@ -112,30 +100,6 @@ class Knight(ChessPiece):
         row_diff = abs(start_row - end_row)
         col_diff = abs(start_col - end_col)
         return row_diff * col_diff == 2 and self.can_move_to(end, board)  # Knight moves in an L-shape (2, 1) or (1, 2)
-
-class Rook(ChessPiece):
-    def __str__(self):
-        return 'R' if self.color == 'white' else 'r'  # String representation for Rook
-
-    def is_valid_move(self, start, end, board):
-        """
-        Checks if the Rook can move from start to end position.
-        
-        Parameters:
-        start (tuple): Start position (row, col)
-        end (tuple): End position (row, col)
-        board (list): Current state of the board
-        
-        
-        Returns:
-        bool: True if the move is valid, False otherwise
-        """
-        start_row, start_col = start
-        end_row, end_col = end
-        if start_row == end_row or start_col == end_col:
-            return self.can_move_to(end, board)  # Rook moves any number of squares along a row or column
-        return False
-
 
 class Pawn(ChessPiece):
     def __str__(self):
